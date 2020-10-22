7a808931188b325163de1f074734bc41e366a65c
Manuel Schamroth <manuel@ffm.net.au>
2020-10-23 10:32:22 +1100

    Update

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,12 @@
+let country = 'Australia';
+let continent = 'Australasia';
+let population = '25 million';
+
+let isIsland = false;
+
+let language;
+
+
+console.log(continent);
+if (isIsland === true) console.log(country);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,6 @@
+let js = 'amazing';
+console.log(40+8+23-10);
+
+let firstName = "bob";
+
+console.log(firstName);
